8240c79f86fda26d946b2023f43a5a1a5117da48
RigobertoPO <rperez@cobach.edu.mx>
2016-12-01 14:15:19 -0600

    Version 1
    Crear estructura de la aplicación  y BD

---
+++ Default.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/PrincipalSite.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebSQLDataSet.Default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div>
+        <asp:GridView ID="ClientesGridView" runat="server" Width="60%" AutoGenerateSelectButton="True" OnSelectedIndexChanged="ClientesGridView_SelectedIndexChanged"></asp:GridView>
+    </div>
+    <div>
+    
+        <asp:Button ID="NuevoButton" runat="server" Text="Nuevo" OnClick="NuevoButton_Click" />
+        <asp:Button ID="EditarButton" runat="server" Text="Editar" Enabled="False" OnClick="EditarButton_Click" />
+        <asp:Button ID="EliminarButton" runat="server" Text="Eliminar" Enabled="False" OnClick="EliminarButton_Click" />
+       
+    
+    </div>
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebSQLDataSet
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack)
+            { }
+            else
+            {
+                cargarDatos();
+
+            }
+        }
+              private void cargarDatos()
+        {
+             TiendaDataSetTableAdapters.ClientesTableAdapter DsDatos = new TiendaDataSetTableAdapters.ClientesTableAdapter();
+            ClientesGridView.DataSource = DsDatos.GetData();
+            ClientesGridView.DataBind();
+        }
+
+              protected void ClientesGridView_SelectedIndexChanged(object sender, EventArgs e)
+              {
+
+              }
+
+              protected void NuevoButton_Click(object sender, EventArgs e)
+              {
+
+              }
+
+              protected void EditarButton_Click(object sender, EventArgs e)
+              {
+
+              }
+
+              protected void EliminarButton_Click(object sender, EventArgs e)
+              {
+
+              }
+    }
+}
